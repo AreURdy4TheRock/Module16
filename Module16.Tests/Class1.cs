b9e5994f5913b94ec0721ae0e8a005d0ac9aa57e
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-05-25 16:02:54 +0300

    Добавьте файлы проекта.

---
+++ Module16.Tests/Class1.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module16.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test, Description("Тест сложения")]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(1, 3) == 4);
+        }
+
+        [Test, Description("Тест вычитания")]
+        public void SubtractionMustReturnCorrectValue() 
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(10, 4) == 6);
+        }
+
+        [Test, Description("Тест умножения")]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(5, 5) == 25);
+        }
+
+        [Test, Description("Тест деления")]
+        public void DivisionMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(20, 4) == 5);
+        }
+
+        [Test, Description("Тест деления на ноль")]
+        public void DivisionByZeroMustReturnException()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(10, 0));
+        }
+    }
+}
---
+++ Module16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module16
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Additional(1, 2);
+            calculator.Subtraction(10, 4);
+            calculator.Miltiplication(5, 5);
+            calculator.Division(20, 4);
+        }
+    }
+}
